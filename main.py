19ec16229f262f707e4bb9193f4615b4c18e0b81
AdamDee64 <deea7243@gmail.com>
2025-12-25 14:44:12 -0500

    ship and asteroids collide

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -14,8 +18,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    
+    asteroidfield = AsteroidField()
 
     while True:
         log_state()
@@ -23,7 +37,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
 
